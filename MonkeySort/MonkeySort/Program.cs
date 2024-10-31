2d0d4fa16fe19798646dc9fbfb84a1a4b779eaba
kto765 <nastya.vakhitova700@mail.ru>
2024-10-31 12:23:22 +0300

    остальные работы

---
+++ MonkeySort/MonkeySort/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class MonkeySort
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числа с пробелами:");
+        string input = Console.ReadLine();
+
+        // Разбиваем строку на массив строк
+        string[] numbersStr = input.Split(' ');
+
+        // Преобразуем массив строк в массив целых чисел
+        int[] numbers = new int[numbersStr.Length];
+        for (int i = 0; i < numbersStr.Length; i++)
+        {
+            numbers[i] = int.Parse(numbersStr[i]);
+        }
+
+        // Сортировка обезьянкой
+        MonkeySortArray(numbers);
+
+        // Вывод отсортированного массива
+        Console.WriteLine("Отсортированный массив:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+
+    // Функция сортировки обезьянкой
+    private static void MonkeySortArray(int[] array)
+    {
+        Random random = new Random();
+        int n = array.Length;
+
+        // Проводим сортировку до тех пор, пока массив не будет отсортирован
+        while (!IsSorted(array))
+        {
+            // Случайно выбираем два индекса
+            int i = random.Next(n);
+            int j = random.Next(n);
+
+            // Меняем местами элементы
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    // Проверка, отсортирован ли массив
+    private static bool IsSorted(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                return false; // Массив не отсортирован
+            }
+        }
+        return true; // Массив отсортирован
+    }
+}
+
+
---
+++ RecursiveSort/RecursiveSort/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числа для сортировки, разделенные пробелами:");
+        string input = Console.ReadLine();
+
+        // Преобразуем строку ввода в массив целых чисел
+        string[] inputArray = input.Split(' ');
+        int[] numbers = Array.ConvertAll(inputArray, int.Parse);
+
+        // Выводим исходный массив
+        Console.WriteLine("Исходный массив: " + string.Join(" ", numbers));
+
+        // Сортируем массив
+        QuickSort(numbers, 0, numbers.Length - 1);
+
+        // Выводим отсортированный массив
+        Console.WriteLine("Отсортированный массив: " + string.Join(" ", numbers));
+    }
+
+    static void QuickSort(int[] array, int low, int high)
+    {
+        if (low < high)
+        {
+            // Находим индекс разделителя
+            int pivotIndex = Partition(array, low, high);
+
+            // Рекурсивно сортируем элементы до и после разделителя
+            QuickSort(array, low, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    static int Partition(int[] array, int low, int high)
+    {
+        int pivot = array[high]; // Берем последний элемент как опорный
+        int i = low - 1; // Индекс меньшего элемента
+
+        for (int j = low; j < high; j++)
+        {
+            // Если текущий элемент меньше или равен опорному
+            if (array[j] <= pivot)
+            {
+                i++;
+                Swap(array, i, j); // Меняем местами
+            }
+        }
+        Swap(array, i + 1, high); // Ставим опорный элемент на его правильное место
+        return i + 1;
+    }
+
+    static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
