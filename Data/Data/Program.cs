368be819adb1fec2041afe2a5c584511cd42b0df
kto765 <nastya.vakhitova700@mail.ru>
2024-11-13 14:30:45 +0300

    Даты

--- Data/Data/Program.cs
+++ Data/Data/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-public class Program
+﻿public class Program
 {
     public static void Main()
     {
--- Data/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f8e0f7238fcc4110e0c58fa0e7f16202819b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
